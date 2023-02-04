47146bad1b24261d230022580ae427bb54516ab2
Jeremy Cheng <81793294+Jeremy-Cheng-stack@users.noreply.github.com>
2023-02-03 21:49:30 -0500

    Backend Complete

--- Work/instawork/base/views.py
+++ Work/instawork/base/views.py
@@ -6,27 +6,6 @@ from .forms import MemberForm
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'home.html')
-
-
-def add_team_member(request):
-    if request.method == 'POST':
-        form = MemberForm(request.POST)
-        if form.is_valid():
-            team_member = form.save()
-            team_member.save()
-            return redirect('team_member_list')
-
-
-def addpage(request):
-    return render(request, 'addpage.html')
-
-
-def editpage(request):
-    return render(request, 'editpage.html')
-
-
 class TeamMemberListView(ListView):
     model = Member
     template_name = 'Home.html'
@@ -55,9 +34,5 @@ class TeamMemberUpdateView(UpdateView):
 
 class TeamMemberDeleteView(DeleteView):
     model = Member
-    form_class = MemberForm
     template_name = 'Deletepage.html'
-
-    def form_valid(self, form):
-        form.save()
-        return redirect('team_member_list')
+    success_url = "/"
